cbaa8503532a7f266f3bbe815e580d630fe843c1
evgeny_sem163 <evg82tol@gmail.com>
2023-01-11 19:53:39 +0300

    Первая часть заданий с for готова!

---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+    }
+    // задание 1
+    private static void task1() {
+        System.out.println(" Задание 1");
+        for (int i = 1; i < 11; i++) { // С помощью цикла for выводим в консоль все числа от 1 до 10.
+            System.out.println(i);
+        }
+    }
+    private static void task2() {
+        // задание 2
+        System.out.println(" Задание 2 ");
+        for (int i = 10; i > 0; i--) { // С помощью цикла for выводим в консоль все числа от 10 до 1.
+            System.out.println(i);
+        }
+    }
+    private static void task3() {
+        System.out.println(" Задание 3 ");
+        for (int i = 0; i < 17; i++) { // Вывожу в консоль все четные числа от 0 до 17.
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+    private static void task4() {
+        System.out.println(" Задание 4");
+        for (int i = 10; i > -11; i--) { // Вывожу в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
+            System.out.println(i);
+        }
+    }
+    private static void task5() {
+        System.out.println(" Задание 5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            // надонаписать программу, которая выводит в консоль все високосные года, начиная с 1904 года до 2096.
+            System.out.println("Високосный год " + i);
+        }
+    }
+    private static void task6() {
+        System.out.println(" Задание 6 ");
+        //Пишем  программу, которая выводит в консоль последовательность чисел:
+        //7 14 21 28 35 42 49 56 63 70 77 84 91 98
+        for (int i = 7; i < 99; i += 7) {
+
+            System.out.println(i);
+        }
+    }
+    private static void task7() {
+        System.out.println(" Задание 7");
+// Пишем программу, которая выводит в консоль последовательность чисел:
+//1 2 4 8 16 32 64 128 256 512
+        for (int i = 1; i < 513; i *= 2) {
+            System.out.println(i);
+        }
+    }
+    private static void task8() {
+        System.out.println(" Задание 8 ");
+//Подсчитываем сумму годовых накоплений, если каждый месяц будем откладывать по 29 000 рублей «в банку».
+        int savings = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+    }
+    private static void task9() {
+        System.out.println(" Задание 9 ");
+        // Подсчитываем сумму годовых накоплений при размешении в банк под проценты — 12% годовых
+        int savings1 = 29000;
+        int total1 = 0;
+        for (int i = 0; i < 12; i++) {
+            total1 = total1 + total1 /100;
+            total1 = total1 + savings1;
+            System.out.println("Месяц " + i + " Итого " + total1);
+        }
+    }
+    private static void task10() {
+        System.out.println(" Задание 10 ");
+        // пишем программу для вывода таблицы умножения на 2:
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " * 2 = " + i * 2);
+        }
+    }
+}
+
+
+
+
+
+
+
+
